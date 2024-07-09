666fc7b16a4cb4a6fc78e2b44d8057bdba08dac8
Sb99CODE <138096045+Sb99CODE@users.noreply.github.com>
2024-07-09 14:05:08 +0530

    clienturl

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
-    "https://cha-tur-client.vercel.app",
-    "https://cha-tur-client-difmm6p3y-sujals-projects-46f211ea.vercel.app",
+    "https://cha-turrr-client.vercel.app",
+    "https://cha-turrr-client-git-master-sujals-projects-46f211ea.vercel.app",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
